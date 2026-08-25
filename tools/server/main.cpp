f43c5e4e8765286754d2bcd3b405cd4d74ba17ee
dai8647 <dai8647@gmail.com>
2026-08-25 09:55:18 +0900

    server : unbuffer stderr

--- tools/server/main.cpp
+++ tools/server/main.cpp
@@ -1,5 +1,9 @@
+#include <cstdio>
+
 int llama_server(int argc, char ** argv);
 
 int main(int argc, char ** argv) {
+    // unbuffered stderr so fatal messages survive abnormal termination
+    setvbuf(stderr, NULL, _IONBF, 0);
     return llama_server(argc, argv);
 }
